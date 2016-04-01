0d18564facf4cb04b53c2bf87fdd4b96b859d98b
KevinCamp <kevinjamescamp@gmail.com>
2016-04-01 17:55:34 -0400

    Inheritance FTW.

--- src/pacman/Main.java
+++ src/pacman/Main.java
@@ -1,11 +1,17 @@
 package pacman;
 
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.stage.Stage;
+
 /**
  * Created by on 2016-04-01.
  */
-public class Main {
-
+public class Main extends Application {
+    public void start(Stage stage){
+        
+    }
     public static void main(String[] args) {
-
+       launch(args);
     }
 }
