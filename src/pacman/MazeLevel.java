3964040efac8ad1c8974752c62f86a1b12848e4f
KevinCamp <kevinjamescamp@gmail.com>
2016-04-09 15:42:51 -0400

    Added stuff to PacMan, and stuff to MazeLevel(some of this will need to be changed to accommodate the new smaller map.

--- src/pacman/MazeLevel.java
+++ src/pacman/MazeLevel.java
@@ -10,6 +10,8 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.shape.SVGPath;
 import javafx.util.Duration;
 
@@ -18,6 +20,7 @@ import javafx.util.Duration;
  */
 public class MazeLevel {
 
+    Rectangle[] mazeRect;
 
     //MazeScene
    private Scene maze;
@@ -51,9 +54,146 @@ public class MazeLevel {
 
     public MazeLevel()
     {
+        Rectangle GamesSpace = new Rectangle(1199, 799);
+//Spawn
+Rectangle l1 = new Rectangle(300, 40);
+l1.setFill(Color.BLUE);
+l1.setTranslateY(290);
+Rectangle l2 = new Rectangle(40, 210);
+l2.setFill(Color.BLUE);
+l2.setTranslateY(375);
+l2.setTranslateX(265);
+Rectangle l3 = new Rectangle(40, 210);
+l3.setFill(Color.BLUE);
+l3.setTranslateY(375);
+l3.setTranslateX(-265);
+//right of spawn
+Rectangle l4 = new Rectangle(100, 200);
+l4.setFill(Color.BLUE);
+l4.setTranslateY(200);
+l4.setTranslateX(450);
+Rectangle l5 = new Rectangle(100, 300);
+l5.setFill(Color.BLUE);
+l5.setTranslateY(-168);
+l5.setTranslateX(450);
+//left of spawn
+Rectangle l6 = new Rectangle(100, 200);
+l6.setFill(Color.BLUE);
+l6.setTranslateY(200);
+l6.setTranslateX(-450);
+Rectangle l7 = new Rectangle(100, 300);
+l7.setFill(Color.BLUE);
+l7.setTranslateY(-168);
+l7.setTranslateX(-450);
+// middle
+Rectangle l9 = new Rectangle(40, 185);
+l9.setFill(Color.BLUE);
+l9.setTranslateY(88);
+l9.setTranslateX(265);
+Rectangle l10 = new Rectangle(40, 185);
+l10.setFill(Color.BLUE);
+l10.setTranslateY(88);
+l10.setTranslateX(-265);
+//ghost spawn
+Rectangle l8 = new Rectangle(300, 40);
+l8.setFill(Color.BLUE);
+l8.setTranslateY(160);
+Rectangle l12 = new Rectangle(40, 120);
+l12.setFill(Color.BLUE);
+l12.setTranslateY(95);
+l12.setTranslateX(130);
+Rectangle l13 = new Rectangle(40, 120);
+l13.setFill(Color.BLUE);
+l13.setTranslateY(95);
+l13.setTranslateX(-130);
+Rectangle l16 = new Rectangle(100, 40);
+l16.setFill(Color.BLUE);
+l16.setTranslateY(15);
+l16.setTranslateX(100);
+Rectangle l17 = new Rectangle(100, 40);
+l17.setFill(Color.BLUE);
+l17.setTranslateY(15);
+l17.setTranslateX(-100);
+//top
+Rectangle l14 = new Rectangle(40, 220);
+l14.setFill(Color.BLUE);
+l14.setTranslateY(-210);
+l14.setTranslateX(265);
+Rectangle l15 = new Rectangle(40, 220);
+l15.setFill(Color.BLUE);
+l15.setTranslateY(-210);
+l15.setTranslateX(-265);
+Rectangle l18 = new Rectangle(300, 100);
+l18.setFill(Color.BLUE);
+l18.setTranslateY(-350);
+Rectangle l19 = new Rectangle(300, 100);
+l19.setFill(Color.BLUE);
+l19.setTranslateY(-150);
+
+mazeRect = new Rectangle[19];
+for(int i =1; i < 19;i++){
+    switch(i){
+        case 1:
+            mazeRect[i] = l1;
+            break;
+        case 2:
+            mazeRect[i] = l2;
+            break;
+        case 3:
+            mazeRect[i] = l3;
+            break;
+        case 4:
+            mazeRect[i] = l4;
+            break;
+        case 5:
+            mazeRect[i] = l5;
+            break;
+        case 6:
+            mazeRect[i] = l6;
+            break;
+        case 7:
+            mazeRect[i] = l7;
+            break;
+        case 8:
+            mazeRect[i] = l8;
+            break;
+        case 9:
+            mazeRect[i] = l9;
+            break;
+        case 10:
+            mazeRect[i] = l10;
+            break;
+        case 11:
+            //mistake, no rectangle l11
+            mazeRect[i] = l19;
+            break;
+        case 12:
+            mazeRect[i] = l12;
+            break;
+        case 13:
+            mazeRect[i] = l13;
+            break;
+        case 14:
+            mazeRect[i] = l14;
+            break;
+        case 15:
+            mazeRect[i] = l15;
+            break;
+        case 16:
+            mazeRect[i] = l16;
+            break;
+        case 17:
+            mazeRect[i] = l17;
+            break;
+        case 18:
+            mazeRect[i] = l18;
+            break;
+        
+    }
+}
         this.paneMaze = new Pane();
 
-        this.spriteImage = getSpriteImagePath("assets/img/PacManSprite.png");
+        this.spriteImage = getSpriteImagePath("JavaFXPacMan/assets/img/PacManSprite.png");
 
         this.imgMaze = new ImageView(this.spriteImage);
         this.imgMaze.setViewport(getMazeSprite());
@@ -118,4 +258,6 @@ public class MazeLevel {
     public Scene getScene() {
         return this.maze;
     }
+    
+    
 }
--- src/pacman/PacMan.java
+++ src/pacman/PacMan.java
@@ -1,11 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package pacman;
 
+import javafx.geometry.Bounds;
 import javafx.geometry.Rectangle2D;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 
 /**
  *
@@ -16,6 +16,8 @@ public class PacMan extends MovableEntity
     boolean isVisible;
     String name;
     boolean isDead;
+    Circle c1;
+    Rectangle[] mazeRect;
 
     public PacMan ()
     {
@@ -27,7 +29,6 @@ public class PacMan extends MovableEntity
         this.name = name;
     }
 
-    //TODO Insert Correct Sprite Bounds
     @Override
     public Rectangle2D getSprite() {
         return new Rectangle2D(607, 344, 32,32);
@@ -55,4 +56,77 @@ public class PacMan extends MovableEntity
         this.isDead =true;
         this.isVisible=false;
     }
+    
+    public boolean collisionDetection(){
+    for(int i=1;i<mazeRect.length; i++){
+Bounds wallObject = mazeRect[i].getBoundsInParent();
+    
+Bounds circleObject = c1.getBoundsInParent();
+
+if(circleObject.intersects(wallObject)){
+    return true;
+}
+}
+    return false;
+}
+
+
+public void moveToLeft (Circle circle)
+{
+if(collisionDetection()==true){
+    circle.setTranslateX(circle.getTranslateX() +15);
+}
+else{
+circle.setTranslateX(circle.getTranslateX() - 10);
+}
+}
+
+public void moveToRight (Circle circle)
+{
+if(collisionDetection()==true){
+    circle.setTranslateX(circle.getTranslateX() -15);
+}
+else{
+circle.setTranslateX(circle.getTranslateX() + 10);
+}
+}
+
+public void moveDown (Circle circle)
+{
+if(collisionDetection()==true){
+    circle.setTranslateY(circle.getTranslateY() -15);
+}
+else{
+circle.setTranslateY(circle.getTranslateY() + 10);
+}
+}
+
+public void moveUp (Circle circle)
+{
+if(collisionDetection()==true){
+    circle.setTranslateY(circle.getTranslateY() +15);
+}
+else{
+circle.setTranslateY(circle.getTranslateY() - 10);
+}
+}
+
+public void getKeyboardInput (KeyEvent e, Circle circle)
+{
+switch (e.getCode()) {
+case A:
+moveToLeft(circle);
+break;
+case D:
+moveToRight(circle);
+break;
+case S:
+moveDown(circle);
+break;
+case W:
+moveUp(circle);
+break;
+}
+}
+
 }
